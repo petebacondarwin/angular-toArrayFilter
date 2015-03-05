776a1d0cd699ec67a263426dea53cffd24921ffa
Kurt Preston <bitbucket@kurtpreston.com>
2015-03-05 13:31:06 -0600

    Adding support for non-object elements

--- toArrayFilter.js
+++ toArrayFilter.js
@@ -2,14 +2,21 @@ angular.module('angular-toArrayFilter', [])
 
 .filter('toArray', function () {
   return function (obj, addKey) {
-    if (!obj) return obj;
+    if ( obj === undefined ) {
+      return undefined;
+    }
+
     if ( addKey === false ) {
       return Object.keys(obj).map(function(key) {
         return obj[key];
       });
     } else {
       return Object.keys(obj).map(function (key) {
-        return Object.defineProperty(obj[key], '$key', { enumerable: false, value: key});
+        if (typeof obj[key] === "object") {
+          return Object.defineProperty(obj[key], '$key', { enumerable: false, value: key});
+        } else {
+          return obj[key];
+        }
       });
     }
   };
